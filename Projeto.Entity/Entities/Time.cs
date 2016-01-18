d8b3ce47fa791be42e4825978e7d2f72be66627b
taysontey <taysontey@gmail.com>
2016-01-18 08:11:31 -0200

    Entity Mapping(1)

---
+++ Projeto.Entity/Entities/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entity.Entities
+{
+    public class Time
+    {
+        public int IdTime { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataFundacao { get; set; }
+    }
+}
