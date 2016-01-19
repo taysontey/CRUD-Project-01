5176a3ec60bbd95ddf8fd44f6e600df1f74b874c
taysontey <taysontey@gmail.com>
2016-01-19 09:14:24 -0200

    Controller/Model Cadastro(1)

---
+++ Projeto.Web/Controllers/JogadorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.Web.Controllers
+{
+    public class JogadorController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto.Web/Models/TimeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Web.Models
+{
+    public class TimeModelCadastro
+    {
+        [Required(ErrorMessage = "Por favor, informe o nome do time.")]
+        [Display(Name = "Nome:")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a data de fundação do time.")]
+        [Display(Name = "Data de Fundação:")]
+        public DateTime DataFundacao { get; set; }
+    }
+}
