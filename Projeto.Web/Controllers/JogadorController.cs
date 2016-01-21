b1124d7124f6537682338025ffe2b1c1399680e8
taysontey <taysontey@gmail.com>
2016-01-21 19:01:10 -0200

    Cadastrar Time(1)

--- Projeto.Web/Controllers/JogadorController.cs
+++ Projeto.Web/Controllers/JogadorController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Projeto.Entity.Entities;
+using Projeto.DAL.Persistence;
+using Projeto.Web.Models;
 
 namespace Projeto.Web.Controllers
 {
@@ -11,7 +14,7 @@ namespace Projeto.Web.Controllers
         [AllowAnonymous]
         public ActionResult Cadastro()
         {
-            return View();
+            return View(new JogadorModelCadastro());
         }
 
         [AllowAnonymous]
--- Projeto.Web/Controllers/TimeController.cs
+++ Projeto.Web/Controllers/TimeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Projeto.Entity.Entities;
+using Projeto.DAL.Persistence;
+using Projeto.Web.Models;
 
 namespace Projeto.Web.Controllers
 {
@@ -17,6 +20,36 @@ namespace Projeto.Web.Controllers
         [AllowAnonymous]
         public ActionResult Consulta()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(TimeModelCadastro model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Time t = new Time();
+                    TimeDal d = new TimeDal();
+
+                    t.Nome = model.Nome;
+                    t.DataFundacao = model.DataFundacao;
+
+                    d.Insert(t);
+
+                    ViewBag.Mensagem = "Time " + t.Nome + ", cadastrado com sucesso.";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Preencha os campos corretamente.";
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Mensagem = e.Message;
+            }
             return View();
         }
     }
